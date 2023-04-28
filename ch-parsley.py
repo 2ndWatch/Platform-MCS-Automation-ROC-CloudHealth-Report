cd70d105e4afd6f336dadc3d8fc28b39ef2d03a5
flaptop23 <awittung@2ndwatch.com>
2023-04-27 21:57:11 -0700

    Initial Commit for parsing downloaded CSVs. This is currently a placeholder.

---
+++ ch-parsley.py
@@ -0,0 +1,5 @@
+import csv
+with open('cypherworxmain-us-east-1-old-images.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
