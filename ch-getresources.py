b0f49f8538911842019fcdbfc7f9d7a948cd876e
Alex <aeversmeyer@2ndwatch.com>
2023-04-29 20:33:06 -0600

    add partial elastic ip function

--- ch-getresources.py
+++ ch-getresources.py
@@ -3,10 +3,11 @@ from modules import ec2_old_images as eold
 from modules import ec2_old_snapshots as esnap
 from modules import rds_old_snapshots as rsnap
 
-profile_name = "eomain"
-region_name = "us-east-2"
-account_number = '269180092429'
+profile_name = "cypherworxmain"
+region_name = "us-east-1"
+account_number = '372356060901'
 
+report_date = '2023-04-24'
 three_month_cutoff_date = '2023-01-24'
 
 session = boto3.Session(profile_name=profile_name, region_name=region_name)
@@ -28,18 +29,18 @@ ct = session.client('cloudtrail')
 # needs a wait condition for cloudtrail lookup_events API call (limit two calls per second)
 
 print(f'Starting Cloud Health validation for {profile_name} in {region_name}.\n')
-print('Getting EC2 images older than 3 months...')
-all_old, valid_old, img_snaps = eold.get_old_images(ec2, account_number, three_month_cutoff_date,
-                                                    profile_name, region_name)
-print(f'   Number of old images: {len(all_old)}')
-print(f'   Number of valid old images: {len(valid_old)}')
-
-print('Getting EBS snapshots older than 3 months...')
-snapshot_count, valid_snapshot_count = esnap.get_old_snapshots(ec2, img_snaps, account_number,
-                                                               three_month_cutoff_date, profile_name, region_name)
-print(f'   Number of old EBS snapshots: {snapshot_count}')
-print(f'   Number of valid old EBS snapshots: {valid_snapshot_count}')
-
-print('Getting RDS snapshots older than 3 months...')
-db_snapshot_count = rsnap.get_old_rds_snaps(rds, three_month_cutoff_date, profile_name, region_name)
-print(f'   Number of valid old RDS snapshots: {db_snapshot_count}')
+# print('Getting EC2 images older than 3 months...')
+# all_old, valid_old, img_snaps = eold.get_old_images(ec2, account_number, three_month_cutoff_date,
+#                                                     profile_name, region_name)
+# print(f'   Number of old images: {len(all_old)}')
+# print(f'   Number of valid old images: {len(valid_old)}')
+#
+# print('Getting EBS snapshots older than 3 months...')
+# snapshot_count, valid_snapshot_count = esnap.get_old_snapshots(ec2, img_snaps, account_number,
+#                                                                three_month_cutoff_date, profile_name, region_name)
+# print(f'   Number of old EBS snapshots: {snapshot_count}')
+# print(f'   Number of valid old EBS snapshots: {valid_snapshot_count}')
+#
+# print('Getting RDS snapshots older than 3 months...')
+# db_snapshot_count = rsnap.get_old_rds_snaps(rds, three_month_cutoff_date, profile_name, region_name)
+# print(f'   Number of valid old RDS snapshots: {db_snapshot_count}')
---
+++ modules/ec2_elastic_ips.py
@@ -0,0 +1,62 @@
+# TODO: function for elastic IPs unattached for 4+ weeks (will require CloudTrail API call)
+# ec2.describe_addresses
+# ct.lookup_events
+# needs a wait condition for cloudtrail lookup_events API call (limit two calls per second)
+
+import csv
+import datetime
+import time
+
+
+def get_old_unattached_eips(ec2_client, ct_client, report_date, profile_name, region_name):
+    """
+
+    :return:
+    """
+    valid_count = 0
+    is_next = None
+    with open(f'{profile_name}-{region_name}-elastic-ips.csv', 'w') as csvfile:
+        # adding 'Unattached At' field would require an additional call to AWS Config; might be extraneous
+        fields = ['Public IP']
+        writer = csv.writer(csvfile)
+        writer.writerow(fields)
+        while True:
+            if is_next:
+                # TODO: check these parameters & token name
+                response = ec2_client.describe_addresses(MaxResults=400, NextToken=is_next)
+            else:
+                response = ec2_client.describe_addresses(MaxResults=400)
+
+            all_ips = response['Addresses']  # list
+
+            for ip in all_ips:
+                ip_public = ip['PublicIp']
+                unattached_and_old = True
+                if 'AssociationId' in ip.keys():
+                    alloc_id = ip['AssociationId']
+
+                    # TODO: add parameters
+                    ct_response = ct_client.lookup_events()
+
+                    # TODO: parse response
+                    # yyy = xxx['eventTime'][:10]
+
+                    # TODO: figure out datetime to subtract 4 weeks from report date
+                    # if event_time > 'report date minus four weeks':
+                    #     unattached_and_old = False
+
+                    print('   CloudTrail API call for EIP made, waiting 31 seconds...')
+                    time.sleep(31)
+                if unattached_and_old:
+                    row = [ip_public]
+                    writer.writerows([row])
+                    valid_count += 1
+
+            try:
+                is_next = response['NextToken']
+            except KeyError:
+                break
+
+    csvfile.close()
+
+    return
